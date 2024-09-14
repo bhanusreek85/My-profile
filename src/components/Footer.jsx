703e91b4d4ba12662e16474f1a18ed37c747e893
Bhanusree <banusreek@gmail.com>
2024-09-13 17:46:47 -0700

    Home page changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-light text-center py-3">
+      <div className="container">
+        <a href="https://github.com/your-github-bhanusreek85" target="_blank" rel="noopener noreferrer" className="text-light">
+          <i className="fab fa-github fa-2x"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/bhanu-sree-csm®-b560417a/" target="_blank" rel="noopener noreferrer" className="text-light mx-2">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+        <p className="mt-2">© 2023 bhanusreek</p>
+      </div>
+    </footer>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,8 +14,16 @@ export default function HomePage() {
     <div className="container pt-4">
          <h1 className="gradient-text">Welcome to My Profile</h1>
          <h2 className="gradient-text">SAP Digital Transformation Architect</h2>
-         <h3>About Me</h3>
-         <img src={sampleImage} alt="Sample" className="small-image float-right" />
+        <div className="row">
+        <div className="col-md-8">
+         <h3 className="text-primary">About Me</h3>
+         <p className="text-secondary">SAP certified Lead ABAP developer with over 18 years of in-depth hands on experience in Design, Development, Testing, Implementation and Support for API’s, Reports, Interfaces, Conversions, Forms, Enhancements and Workflows in various SAP ECC Modules like SD, MM, FI, MM, HR and FSCD. Expertise in ODATA services and third-party application API integration in a cloud-based environment. Strong knowledge of SAP ABAP on HANA. Experience in managing onsite and offshore development team, coordinating with Business Analysts and Users, documenting business requirements, developing functional design documents. Excellent team lead as well as team player with strong interpersonal and communication skills.</p>
+         </div>
+         <div className="col-md-4">
+         <img src={sampleImage} alt="Sample" className="img-fluid small-image" />
+         {/* </div> */}
+         </div>
+         </div>
       <ul className="list-group list-group">
       
       </ul>

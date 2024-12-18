e279ea0f0cb24f136be065320df1e3220391beef
Bhanusree <banusreek@gmail.com>
2024-12-18 08:15:24 -0800

    change text color under about me

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ export default function HomePage() {
             <div className="row">
               <div className="col-md-6">
                 <h3 className="text-primary text-center">About Me</h3>
-                <p className="text-secondary fw-bold">
+                <p className="text-light fw-bold">
                   SAP certified Lead ABAP developer with over 18 years of in-depth hands on experience in designing and implementing efficient, scalable and innovative solutions. My Expertise lies in Development, Testing, Implementation and Support for API’s, Reports, Interfaces, Conversions, Forms, Enhancements and Workflows in various SAP ECC Modules like SD, MM, FI, MM, HR and FSCD. Expertise in ODATA services and third-party application API integration in a cloud-based environment. Strong knowledge of SAP ABAP on HANA. Experience in managing onsite and offshore development team, coordinating with Business Analysts and Users, documenting business requirements, developing functional design documents. Excellent team lead as well as team player with strong interpersonal and communication skills.
                 </p>
               </div>
